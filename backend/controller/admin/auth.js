6a5f2596a0b2433805b782a472139f8018dbe4f8
ShoonLei <pandapanda254.pp@gmail.com>
2021-01-06 17:21:46 +0630

    Validation done

--- backend/controller/admin/auth.js
+++ backend/controller/admin/auth.js
@@ -1,8 +1,10 @@
 const User=require('../../models/user')
 const jwt=require('jsonwebtoken')
+const { validationResult } = require('express-validator')
 
 
 exports.signup=(req,res)=>{
+    
     User.findOne({ email: req.body.email })
     .exec((error,user)=>{
         if(user) return res.status(400).json({
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 const express=require('express')
+const {validateSignInRequest,validateSignUpRequest,isRequestValidated} = require('../validators/validators')
 const { signup, signin, requiredSignin } = require('../controller/auth')
 const router=express.Router()
 const User=require('../models/user')
 
 
-router.post('/signin',signin)
-router.post('/signup',signup)
+
+router.post('/signin',validateSignInRequest,isRequestValidated,signin)
+router.post('/signup',validateSignUpRequest,isRequestValidated,signup)
 router.post('/profile',requiredSignin,(req,res)=>{
     res.status(200).json({
         user:"profile"
